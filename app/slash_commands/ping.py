82a994cd9735bd2b3c2ed6290d0f34eb0288b385
Jelle Kuipers <rj.kuipers@student.alfa-college.nl>
2024-03-06 21:59:25 +0100

    updated hello and new ping command

--- app/slash_commands/ping.py
+++ app/slash_commands/ping.py
@@ -1,16 +1,15 @@
 from discord.ext import commands
 
 # This is a cog, a class that contains commands
-class helloCommandCog(commands.Cog):
+class pingCommandCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
     
     # This is a command, a function that can be called by a user    
-    @commands.slash_command(description="Says hello to the user.")
-    async def hello(self, ctx, name: str = None):
-        name = name or ctx.author.name
-        await ctx.respond(f"Hello {name}!")
+    @commands.slash_command(description="Sends the bot's latency.")
+    async def ping(self, ctx):
+        await ctx.respond(f"Pong! Latency is {round(self.bot.latency * 1000)}ms")
 
 # This function is called in app/slash_commands/__init__.py, and adds the command to the bot
 def setup(bot):
-    bot.add_cog(helloCommandCog(bot))
+    bot.add_cog(pingCommandCog(bot))
